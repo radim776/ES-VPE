496bdb6298ed8ff0ed1a0d21327a0f988b6fcb5f
radim776 <130166108+radim776@users.noreply.github.com>
2026-03-09 15:56:56 +0100

    Add files via upload

--- ES_IDE/Properties/AssemblyInfo.cs
+++ ES_IDE/Properties/AssemblyInfo.cs
@@ -1,1 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("ES IDE")]
+[assembly: AssemblyDescription("Extremly gooS(good) IDE ,easy app code . Free . No code . Easy")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RO INC")]
+[assembly: AssemblyProduct("ES IDE")]
+[assembly: AssemblyCopyright("Copyright © RO INC 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("bf988de2-1e79-4833-9c3b-85404105674f")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize pomocí *, jak je vidět níže:
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly:     AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyFileVersion("0.0.4.0")]
---
+++ ES_IDE/Properties/Settings.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace EventScriptIDE
+{
+    public class AppSettings
+    {
+        [JsonProperty("buildtools_path")]
+        public string BuildToolsPath { get; set; } =
+            @"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools";
+
+        [JsonProperty("discordrpc")]
+        public bool DiscordRpc { get; set; } = false;
+    }
+
+    public static class SettingsManager
+    {
+        public static readonly string AppData =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                         "EventScriptIDE");
+
+        public static readonly string SettingsPath =
+            Path.Combine(AppData, "Settings.json");
+
+        public static readonly string ProjectsRoot =
+            Path.Combine(AppData, "Projects");
+
+        public static readonly string ExtensionsDir =
+            Path.Combine(AppData, "Extensions");
+
+        public static AppSettings Load()
+        {
+            try
+            {
+                if (File.Exists(SettingsPath))
+                {
+                    var json = File.ReadAllText(SettingsPath);
+                    return JsonConvert.DeserializeObject<AppSettings>(json) ?? new AppSettings();
+                }
+            }
+            catch { /* fall through */ }
+            return new AppSettings();
+        }
+
+        public static void Save(AppSettings s)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(SettingsPath)!);
+            File.WriteAllText(SettingsPath, JsonConvert.SerializeObject(s, Formatting.Indented));
+        }
+    }
+}
