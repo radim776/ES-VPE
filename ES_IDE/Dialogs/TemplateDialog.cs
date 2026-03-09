f5a076a43e79a4b43f58c6fbb5def11a0f65a1c8
radim776 <130166108+radim776@users.noreply.github.com>
2026-03-09 16:50:48 +0100

    Update TemplateDialog.cs

--- ES_IDE/Dialogs/TemplateDialog.cs
+++ ES_IDE/Dialogs/TemplateDialog.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +16,34 @@ namespace EventScriptIDE.Dialogs
         public TemplateDialog()
         {
             InitializeComponent();
+            panel1.Click += Panel1_Click;
+            panel2.Click += Panel2_Click;
+            radioButton1.Click += RadioButton1_Click;
+            radioButton2.Click += RadioButton2_Click;
+        }
+
+        private void RadioButton2_Click(object sender, EventArgs e)
+        {
+            Result = 2;
+            radioButton1.Checked = false;
+        }
+
+        private void RadioButton1_Click(object sender, EventArgs e)
+        {
+            Result = 1;
+            radioButton2.Checked = false;
+        }
+
+        private void Panel2_Click(object sender, EventArgs e)
+        {
+            Result = 2;
+            radioButton2.Checked = true;
+        }
+
+        private void Panel1_Click(object sender, EventArgs e)
+        {
+            Result = 1;
+            radioButton1.Checked = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -25,12 +53,12 @@ namespace EventScriptIDE.Dialogs
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            Result = 1;
+           
         }
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            Result = 2;
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
