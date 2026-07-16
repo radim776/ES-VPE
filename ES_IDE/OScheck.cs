1e389c91d98f16f359b1be5d9fdac7a96537796b
radim776 <130166108+radim776@users.noreply.github.com>
2026-07-16 15:10:46 +0200

    add missing files

---
+++ ES_IDE/OScheck.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EventScriptIDE
+{
+	public static class OSChecker
+	{
+		static readonly string[] SupportedOSes =
+		{
+			"Windows XP",
+			"Windows Vista",
+			"Windows 7",
+			"Windows 8",
+			"Windows 8.1",
+			"Windows 10"
+		};
+
+		public static bool CheckOS()
+		{
+			bool supported = false;
+			string productName = Registry.GetValue(
+				@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion",
+				"ProductName",
+				null
+			)?.ToString();
+
+			if (string.IsNullOrEmpty(productName))
+				return false;
+			
+			string osName = Regex.Replace(productName, @"^Microsoft\s+", "");
+			
+			osName = Regex.Replace(
+				osName,
+				@"\s+(Home|Pro|Professional|Enterprise|Education|IoT|LTSC|LTSB|N|KN|K|Core|Single Language).*",
+				"",
+				RegexOptions.IgnoreCase
+			);
+
+			string buildStr = Registry.GetValue(
+				@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion",
+				"CurrentBuildNumber",
+				null
+			)?.ToString();
+
+			int build = 0;
+			int.TryParse(buildStr, out build);
+
+			if (build>=0 && build<=20000)
+			{
+				supported = SupportedOSes.Any(v => osName.Contains(v));
+			}
+
+			return supported;
+		}
+	}
+}
